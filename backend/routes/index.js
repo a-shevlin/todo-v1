48e20634c72f54ae763b5e55c2bd4d67f3d3b623
a-shev <alexshevlin1@gmail.com>
2023-02-11 22:26:15 -0800

    add protected routes with checkAuth.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,15 @@ import express from 'express';
 import taskRoutes from './tasks.js';
 import authRoutes from './auth.js';
 import userRoutes from './users.js';
+import checkAuth from '../utils/checkAuth.js';
 
 const router = express.Router();
 
 // REQUESTS FOR AUTH
 router.use('/auth', authRoutes);
-router.use('/tasks', taskRoutes);
-router.use('/users', userRoutes)
+
+// PROTECTED ROUTE
+router.use('/tasks', checkAuth, taskRoutes);
+router.use('/users', checkAuth, userRoutes)
 
 export default router;
