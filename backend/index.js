fa173e8bc0885b47885792116e030633990840ae
a-shev <alexshevlin1@gmail.com>
2023-02-11 22:21:53 -0800

    add error handler.

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,13 @@ app.use(cookieParser());
 // routes
 app.use('/api', allRoutes);
 
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(status).json({ status, message, stack: err.stack });
+})
+
 // mongoose connection
 const connectDB = async () => {
   try {
