cc76fe717eca636582207248037b8df6f0ffd59a
a-shev <alexshevlin1@gmail.com>
2023-02-18 00:57:57 -0800

    setup route pathing for app.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import { Routes, Route, Router } from 'react-router-dom';
+import Home from './pages/Home';
+import EditProfile from './pages/EditProfile';
+import Auth from './pages/Auth';
+import PrivateRoutes from './components/PrivateRoutes';
+import Register from './components/auth/Register';
+import Login  from './components/auth/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <React.Fragment>
+      <Toaster
+        position='top-right'
+        toastOptions={{
+          style: {
+            fontSize: '1.8rem'
+          }
+        }}>          
+      </Toaster>
+
+      <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
+        </Route>
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </React.Fragment>
   )
 }
 
-export default App
+export default App;
