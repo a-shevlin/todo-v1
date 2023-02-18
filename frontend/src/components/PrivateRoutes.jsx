8730ca896abf2f0f8bdb794ae6cf8725d9b6d1b3
a-shev <alexshevlin1@gmail.com>
2023-02-18 00:55:39 -0800

    create PrivateRoute for checking user authentication.

---
+++ frontend/src/components/PrivateRoutes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet, Navigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
+function PrivateRoutes() {
+  const { auth } = useAuth();
+
+  if(auth === undefined) return 'Loading.....';
+
+
+  return auth === true ? <Outlet /> : <Navigate to="/auth" />;
+}
+
+export default PrivateRoutes
